b1201801f5ffb2f4d41d8b5c12055a9f82505b46
Harini <nharinirmd@gmail.com>
2025-03-19 22:17:04 +0100

    Replaced var with let

--- scripts/app.js
+++ scripts/app.js
@@ -7,16 +7,16 @@ console.log("Hello World");
  *                                          Exercise 2 :
  *************************************************************************************************/
 console.log("Exercise 2 :");
-var my_name = "Harini";
-var my_age = 35;
-var my_favourite_color = "black";
+let my_name = "Harini";
+let my_age = 35;
+let my_favourite_color = "black";
 console.log("My name is " + my_name + ". I'm " + my_age + " years old and my favourite color is " + my_favourite_color)
 /*************************************************************************************************
  *                                          Exercise 3 :
  *************************************************************************************************/
 console.log("Exercise 3 :");
-var first_number = 20;
-var second_number = 10;
+let first_number = 20;
+let second_number = 10;
 console.log(" \r\n Adding " + first_number + " + " + second_number + " = " + (first_number + second_number));
 console.log(" \r\n Subtracting " + first_number + " - " + second_number + " = " + (first_number - second_number));
 console.log(" \r\n Multiplying " + first_number + " * " + second_number + " = " + (first_number * second_number));
@@ -24,8 +24,8 @@ console.log(" \r\n Dividing " + first_number + " / " + second_number + " = " + (
 /**
  * Used Swtich case with prompt, console.log and alert)
  */
-var select = prompt("What do you want to perform ?  \r\n 1. Addition  \r\n 2. Subtraction  \r\n 3. Multiplication  \r\n 4. Division  \r\n Enter the number of your choice between 1 - 4  \r\n")
-var choice = parseInt(select);
+let select = prompt("What do you want to perform ?  \r\n 1. Addition  \r\n 2. Subtraction  \r\n 3. Multiplication  \r\n 4. Division  \r\n Enter the number of your choice between 1 - 4  \r\n")
+let choice = parseInt(select);
 switch (choice) {
     case 1:
         alert(" \r\n Adding " + first_number + " + " + second_number + " = " + (first_number + second_number));
